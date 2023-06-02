0831f3546bfd66a062ffc5d952e5a58ec88b2919
prabeen <prabeenprabu@gmail.com>
2023-06-02 23:53:26 +0530

    logic optimized

--- app.js
+++ app.js
@@ -1,56 +1,48 @@
-const Computerchoiced = document.getElementById("Computer-choice");
-const userchoiced = document.getElementById("user-choice");
+const computerChoiced = document.getElementById("Computer-choice");
+const userChoiced = document.getElementById("user-choice");
 const Resultdisplay = document.getElementById("result");
 const possiblechoices = document.querySelectorAll("button");
-let computerchoice;
-let userchoice;
+let computerChoice;
+let userChoice;
 
 possiblechoices.forEach((possibleChoice) =>
     possibleChoice.addEventListener("click", (e) => {
-        userchoice = e.target.id;
-        userchoiced.innerHTML = userchoice;
-        generateComputerchoice();
+        userChoice = e.target.id;
+        userChoiced.innerHTML = userChoice;
+        generatecomputerChoice();
         getResult();
     })
 );
 
-function generateComputerchoice() {
-    const random = Math.floor(Math.random() * possiblechoices.length); //or *3
+function generatecomputerChoice() {
+    const random = Math.floor(Math.random() * possiblechoices.length) + 1; //or *3
 
     if (random == 1) {
-        computerchoice = "Rock";
+        computerChoice = "Rock";
     }
     if (random == 2) {
-        computerchoice = "Paper";
+        computerChoice = "Paper";
     }
     if (random == 3) {
-        computerchoice = "Scissor";
+        computerChoice = "Scissor";
     }
-    Computerchoiced.innerHTML = computerchoice;
+    computerChoiced.innerHTML = computerChoice;
 }
 
 function getResult() {
-    if (userchoice == computerchoice) {
+    if (userChoice == computerChoice) {
         Result = "Tie";
-    }
-    if (computerchoice == "Rock" && userchoice == "Paper") {
+        Resultdisplay.style.color = "var(--orange)";
+    } else if (
+        (computerChoice == "Rock" && userChoice == "Paper") ||
+        (computerChoice == "Scissor" && userChoice == "Rock") ||
+        (computerChoice == "Paper" && userChoice == "Scissor")
+    ) {
         Result = "You Win!";
-    }
-    if (computerchoice == "Scissor" && userchoice == "Rock") {
-        Result = "You Win!";
-    }
-
-    if (computerchoice == "Paper" && userchoice == "Scissor") {
-        Result = "You Win!";
-    }
-    if (computerchoice == "Rock" && userchoice == "Scissor") {
+        Resultdisplay.style.color = "var(--green)";
+    } else {
         Result = "You Lost!";
-    }
-    if (computerchoice == "Paper" && userchoice == "Rock") {
-        Result = "You Lost!";
-    }
-    if (computerchoice == "Scissor" && userchoice == "Paper") {
-        Result = "You Lost!";
+        Resultdisplay.style.color = "var(--red)";
     }
 
     Resultdisplay.innerHTML = Result;
